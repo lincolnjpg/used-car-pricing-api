c5d0e7c1495d64c5631df6c6d823486281c4de8b
Lincoln Junior <99145408+lincolnjpg@users.noreply.github.com>
2024-05-26 20:25:21 -0300

    feat: add guard to prevent access when user is not signed in

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post, Patch, Param, Query, Delete, Session } from '@nestjs/common';
+import { Body, Controller, Get, Post, Patch, Param, Query, Delete, Session, UseGuards } from '@nestjs/common';
 import { CreateUserDTO } from './dtos/create-user.dto';
 import { UpdateUserDTO } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
@@ -7,6 +7,7 @@ import { UserDTO } from './dtos/user.dto';
 import { AuthService } from './auth.service';
 import { CurrentUser } from './decorators/current-user.decorator';
 import { User } from './user.entity';
+import { AuthGuard } from 'src/guards/auth.guard';
 
 @Controller('auth')
 @Serialize(UserDTO)
@@ -19,6 +20,7 @@ export class UsersController {
     // }
 
     @Get('/whoami')
+    @UseGuards(AuthGuard)
     whoAmI(@CurrentUser() user: User) {
         return user;
     }
@@ -32,6 +34,7 @@ export class UsersController {
     }
 
     @Post('/signout')
+    @UseGuards(AuthGuard)
     signOut(@Session() session: any) {
         session.userId = null;
     }
@@ -45,21 +48,25 @@ export class UsersController {
     }
 
     @Get('/:id')
+    @UseGuards(AuthGuard)
     findUser(@Param('id') id: string) {
         return this.usersService.findOne(parseInt(id));
     }
 
     @Get()
+    @UseGuards(AuthGuard)
     findAllUsers(@Query('email') email: string) {
         return this.usersService.find(email);
     }
 
     @Delete('/:id')
+    @UseGuards(AuthGuard)
     removeUser(@Param('id') id: string) {
         return this.usersService.remove(parseInt(id));
     }
 
     @Patch('/:id')
+    @UseGuards(AuthGuard)
     updateUser(@Param('id') id: string, @Body() body: UpdateUserDTO) {
         return this.usersService.update(parseInt(id), body);
     }
