9c36bc39b6816ce7be5163922a22ad507a21452f
Lincoln Junior <99145408+lincolnjpg@users.noreply.github.com>
2024-05-26 04:40:37 -0300

    feat: add signup method

---
+++ src/users/auth.service.ts
@@ -0,0 +1,32 @@
+import { BadRequestException, Injectable } from "@nestjs/common";
+import { UsersService } from "./users.service";
+import { scrypt as _scrypt, randomBytes } from "crypto";
+import { promisify } from "util";
+
+const scrypt = promisify(_scrypt);
+
+@Injectable()
+export class AuthService {
+    constructor(private usersService: UsersService) {        
+    }
+
+    async signUp(email: string, password: string) {
+        const users = await this.usersService.find(email);
+
+        if (users.length) {
+            throw new BadRequestException('email in use');
+        }
+
+        const salt = randomBytes(32).toString('hex');
+        const hash = (await scrypt(password, salt, 128)) as Buffer;
+        const result = salt + '.' + hash.toString('hex');
+
+        const user = await this.usersService.create(email, result);
+
+        return user;
+    }
+
+    signIn() {
+
+    }
+}
