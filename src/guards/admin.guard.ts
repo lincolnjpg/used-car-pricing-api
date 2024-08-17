6e168ba7c018b0a905310ab1eead26aa49651add
Lincoln Junior <99145408+lincolnjpg@users.noreply.github.com>
2024-08-17 18:17:05 -0300

    add currentUser to express request interface (and remove some console.logs() too)

--- src/guards/admin.guard.ts
+++ src/guards/admin.guard.ts
@@ -2,7 +2,6 @@ import { CanActivate, ExecutionContext } from '@nestjs/common';
 
 export class AdminGuard implements CanActivate {
   canActivate(context: ExecutionContext) {
-    console.log('ENTROU ADMIN GUARD');
     const request = context.switchToHttp().getRequest();
 
     if (!request.currentUser) {
@@ -10,6 +9,5 @@ export class AdminGuard implements CanActivate {
     }
 
     return request.currentUser.admin;
-    console.log('SAIU ADMIN GUARD');
   }
 }
