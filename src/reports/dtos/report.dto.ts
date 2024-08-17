0e25f60741214eee3f12d3e67b3e366ab70efdfa
Lincoln Junior <99145408+lincolnjpg@users.noreply.github.com>
2024-08-17 02:15:21 -0300

    filtering report exposed fields

---
+++ src/reports/dtos/report.dto.ts
@@ -0,0 +1,31 @@
+import { Expose, Transform } from 'class-transformer';
+
+export class ReportDTO {
+  @Expose()
+  id: number;
+
+  @Expose()
+  price: number;
+
+  @Expose()
+  year: number;
+
+  @Expose()
+  latitude: number;
+
+  @Expose()
+  longitude: number;
+
+  @Expose()
+  make: string;
+
+  @Expose()
+  model: string;
+
+  @Expose()
+  mileage: number;
+
+  @Transform(({ obj }) => obj.user.id)
+  @Expose()
+  userId: number;
+}
